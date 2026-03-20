3829b371cec8cfa62c3041aef7ffca86c9d376c6
ziv <ziv@citi.sinica.edu.tw>
2026-03-21 07:57:54 +0800

    [refactor] mock_server_sending

--- tests/mock_server_sending.c
+++ tests/mock_server_sending.c
@@ -22,14 +22,17 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "e2ees/e2ees_client.h"
 
 #define QUEUE_SIZE 16384
 
 pthread_mutex_t lock;
+pthread_cond_t cond;
 bool running;
 pthread_t thread;
+bool thread_started = false;
 
 E2ees__ProtoMsg *proto_msg_queue[QUEUE_SIZE];
 int proto_msg_queue_insert_head = 0;
@@ -42,32 +45,39 @@ void send_proto_msg(E2ees__ProtoMsg *proto_msg) {
     e2ees__proto_msg__pack(proto_msg, proto_msg_data);
     E2ees__ProtoMsg *cloned_proto_msg = e2ees__proto_msg__unpack(NULL, proto_msg_data_len, proto_msg_data);
 
-    // keep proto_msg in proto_msg_queue
-    // printf("tail: %d", proto_msg_queue_insert_tail);
+    pthread_mutex_lock(&lock);
+    
+    int next_tail = (proto_msg_queue_insert_tail + 1) % QUEUE_SIZE;
+    assert(next_tail != proto_msg_queue_insert_head); // Ensure queue is not full
+    
     proto_msg_queue[proto_msg_queue_insert_tail] = cloned_proto_msg;
-    pthread_mutex_lock(&lock);
-    proto_msg_queue_insert_tail++;
-    if (proto_msg_queue_insert_tail == QUEUE_SIZE)
-        proto_msg_queue_insert_tail = 0;
+    proto_msg_queue_insert_tail = next_tail;
+    
+    pthread_cond_signal(&cond); // Wake up the processing thread
     pthread_mutex_unlock(&lock);
-    assert(proto_msg_queue_insert_tail != proto_msg_queue_insert_head);
-    // printf(" -> %d\n", proto_msg_queue_insert_tail);
 }
 
-static bool has_proto_msg_data() {
-    bool has_data = false;
-    pthread_mutex_lock(&lock);
-    has_data = (proto_msg_queue_insert_tail != proto_msg_queue_insert_head);
-    pthread_mutex_unlock(&lock);
-    return has_data;    
-}
+void *process_outgoing_queue(void *arg) {
+    while (true) {
+        E2ees__ProtoMsg *proto_msg = NULL;
 
-void process_outgoing_queue() {
-    while (running || has_proto_msg_data()) {
-        E2ees__ProtoMsg *proto_msg = proto_msg_queue[proto_msg_queue_insert_head];
+        pthread_mutex_lock(&lock);
+        while (running && proto_msg_queue_insert_head == proto_msg_queue_insert_tail) {
+            pthread_cond_wait(&cond, &lock);
+        }
+
+        if (!running && proto_msg_queue_insert_head == proto_msg_queue_insert_tail) {
+            pthread_mutex_unlock(&lock);
+            break;
+        }
+
+        proto_msg = proto_msg_queue[proto_msg_queue_insert_head];
+        proto_msg_queue[proto_msg_queue_insert_head] = NULL;
+        
+        proto_msg_queue_insert_head = (proto_msg_queue_insert_head + 1) % QUEUE_SIZE;
+        pthread_mutex_unlock(&lock);
 
         if (proto_msg != NULL) {
-            // printf("head: %d", proto_msg_queue_insert_head);
             // send proto_msg to client
             size_t proto_msg_data_len = e2ees__proto_msg__get_packed_size(proto_msg);
             uint8_t proto_msg_data[proto_msg_data_len];
@@ -76,21 +86,12 @@ void process_outgoing_queue() {
 
             // release
             e2ees__proto_msg__free_unpacked(proto_msg, NULL);
-            e2ees__consume_proto_msg_response__free_unpacked(consume_proto_msg_response, NULL);
-            // remove processed proto_msg
-            proto_msg_queue[proto_msg_queue_insert_head] = NULL;
-
-            pthread_mutex_lock(&lock);
-            proto_msg_queue_insert_head++;
-            if (proto_msg_queue_insert_head == QUEUE_SIZE)
-                proto_msg_queue_insert_head = 0;
-            pthread_mutex_unlock(&lock);
-            // printf(" -> %d\n", proto_msg_queue_insert_head);
-        } else {
-            // printf("sleep\n");
-            usleep(100000);
+            if (consume_proto_msg_response != NULL) {
+                e2ees__consume_proto_msg_response__free_unpacked(consume_proto_msg_response, NULL);
+            }
         }
     }
+    return NULL;
 }
 
 void start_mock_server_sending() {
@@ -98,15 +99,27 @@ void start_mock_server_sending() {
         printf("\n mutex init failed\n");
         return;
     }
+    if (pthread_cond_init(&cond, NULL) != 0) {
+        printf("\n cond init failed\n");
+        return;
+    }
     running = true;
-    pthread_create(&thread, NULL, (void *)process_outgoing_queue, "process outgoing queue");
+    if (pthread_create(&thread, NULL, process_outgoing_queue, NULL) == 0) {
+        thread_started = true;
+    }
 }
 
 void stop_mock_server_sending() {
+    pthread_mutex_lock(&lock);
     running = false;
-    if (thread != NULL) {
-        pthread_join(thread, 0);
-        thread = NULL;
+    pthread_cond_broadcast(&cond); // Wake up the thread so it can exit
+    pthread_mutex_unlock(&lock);
+
+    if (thread_started) {
+        pthread_join(thread, NULL);
+        thread_started = false;
     }
+
     pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
 }
